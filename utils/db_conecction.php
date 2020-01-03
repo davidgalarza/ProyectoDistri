4f9a7910d7d7f9020b761777760bab5753a0fcf6
David <Galarza>
2020-01-03 18:38:30 -0500

    Segundo commit

--- utils/db_conecction.php
+++ utils/db_conecction.php
@@ -1,11 +1,8 @@
 <?php
     $servername = "localhost";
-    //$username = "root";
-    //$password = "root";
-    $username = "id12128719_root";
-    $password = "root1234";
-    $database = "id12128719_restaurante";
-    // $database = "Restaurante"; // La buena es Restaurante
+    $username = "root";
+    $password = "root";
+    $database = "Restaurante"; // La buena es Restaurante
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
