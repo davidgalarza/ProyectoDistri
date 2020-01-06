011a30fd67a82f5bbbdbc63e02dd2d915b5ef0fa
David <Galarza>
2020-01-06 00:05:26 -0500

    Mesas ocupadas

---
+++ mesas/listarOcupadas.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: application/json');
+include '../utils/db_conecction.php';
+
+
+$sql = "SELECT * FROM Mesas
+WHERE ESTADO = 'OCUPADA'";
+$sth = $conn->query($sql);
+$rows = array();
+while($r = mysqli_fetch_assoc($sth)) {
+    $rows[] = $r;
+}
+print json_encode($rows);
